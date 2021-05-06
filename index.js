d1e1150202dd8556596e9726e5e8dc791b0e82e4
Connor Holland <supersoccergoalie123@gmail.com>
2021-05-06 17:23:27 -0600

    commit tree 04c347752e9cb30d841911f24186e33634cba546

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ];
+    let result = [];
+
+    for(let word of string.split(" ")) {
+      if (exceptions.includes(word) && word != string.split(" ")[0]) {
+        result.push(word)
+      } else {
+        result.push(word.charAt(0).toUpperCase() + word.slice(1))
+      }
+    }
+    return result.join(" ")
+  }
 }
